a9e2a99426370346849550a93be1043bc9dcab45
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-01-30 19:29:32 +0100

    Create visuals.lua

---
+++ client-side/visuals.lua
@@ -0,0 +1,16 @@
+-- These are the visual tools you could use, made standalone (but took from esx)
+
+-- Classic Help Notification, you can 
+function ShowHelpNotification(msg, thisFrame, beep, duration)
+    AddTextEntry("cn-snippets", msg)
+
+    if thisFrame then
+        DisplayHelpTextThisFrame("cn-snippets", false)
+    else
+        if beep == nil then
+            beep = true
+        end
+        BeginTextCommandDisplayHelp("cn-snippets")
+        EndTextCommandDisplayHelp(0, false, beep, duration or -1)
+    end
+end
