e3b015560db0bc345d2f1a469b6c1ae181919a96
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-01-30 19:15:33 +0100

    Update functions.lua

--- client-side/functions.lua
+++ client-side/functions.lua
@@ -16,3 +16,16 @@ function GetMySex(ped)
         return("f")
     end
 end
+
+-- Return the seat index of the seat that the player is on.
+function GetSeatPedIsIn(ped,car)
+    if car == 0 or car == nil or DoesEntityExist(car) == false then
+        return("You are not in a vehicle baka")
+    else
+        for i=-1,GetVehicleModelNumberOfSeats(GetEntityModel(car)) do
+            if GetPedInVehicleSeat(car,i) == ped then
+                return(tonumber(i)) -- -1 = driver ect..
+            end
+        end
+    end
+end
