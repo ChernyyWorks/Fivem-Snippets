8f26182b356aaf6e2d0e9d6891e1b5f4d4a52aac
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-01-30 19:10:23 +0100

    Update commands.lua

--- client-side/commands.lua
+++ client-side/commands.lua
@@ -8,3 +8,12 @@ RegisterCommand('deleteprop', function()
         end
     end
 end)
+
+-- Easy Full performance vehicle upgrade, don't forget to add your own permissions to access the command
+RegisterCommand("upgrademe",function()
+    local vehicle = GetVehiclePedIsIn(PlayerPedId())
+    SetVehicleMod(vehicle, 11, 3, false) -- Engine
+    SetVehicleMod(vehicle, 12, 2, false) -- Brakes
+    SetVehicleMod(vehicle, 13, 2, false) -- GearBox
+    ToggleVehicleMod(vehicle, 18, true) -- Adds a Turbo
+end)
